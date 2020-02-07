93385edc7419b602dd78cca12462794eb78354fe
Logan Pettit <logan@hoare7.cs.umsl.edu>
2020-02-07 15:58:39 -0600

    added permissions, filetype, uid, gid, i node links options

--- logan.1/FileOptionHandler.c
+++ logan.1/FileOptionHandler.c
@@ -1,28 +1,64 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <string.h>
+#include <pwd.h>
+#include <grp.h>
 
 void getPermissions(int, char[]);
+char getFileType(int);
+int getByteString(long);
 
 char* getOptions(char* path, char optionsArray[], int argCounter) {
 
-    char fileInformation[1024];
+    struct passwd *getpwuid(), *pw_ptr;
+    struct group *getgrgid(), *grp_ptr;
+    char* byteString = "";
     char permissions[10];
+    char fileType;
     int i = 0;
     struct stat sb;
 
     lstat(path, &sb);
 
 
-    for (i; i < argCounter; i++) {
+    for (i = 0; i < argCounter; i++) {
         char opt = optionsArray[i];
 
         switch (opt) {
             case 'p' :
-
                 getPermissions(sb.st_mode, permissions);
+                printf("%-11s", permissions);
+                break;
 
-                printf("%-12s", permissions);
+            case 't' :
+                fileType = getFileType(sb.st_mode);
+                printf("%-2c", fileType);
+                break;
+
+            case 'i' :
+                printf("%-2d", sb.st_nlink);
+                break;
+
+            case 'u' :
+                pw_ptr = getpwuid(sb.st_uid);
+                if(pw_ptr == NULL)
+                    printf("%-5d", sb.st_uid);
+                else
+                    printf("%-10s", pw_ptr->pw_name);
+
+                break;
+
+            case 'g':
+                grp_ptr = getgrgid(sb.st_gid);
+                if(grp_ptr == NULL)
+                    printf("%-5d", sb.st_gid);
+                else
+                    printf("%-10s", grp_ptr->gr_name);
+
+                break;
+
+            case 's' :
+                /*byteString = getByteString((long) sb.st_size);*/
 
                 break;
         }
@@ -30,8 +66,6 @@ char* getOptions(char* path, char optionsArray[], int argCounter) {
 }
 
 void getPermissions(int fileMode, char permissions[]){
-
-    strcpy(permissions, "---------");
 
     strcpy(permissions, "---------");
 
@@ -50,3 +84,38 @@ void getPermissions(int fileMode, char permissions[]){
     if(fileMode & S_IWOTH) permissions[7] = 'w';
     if(fileMode & S_IXOTH) permissions[8] = 'x';
 }
+
+char getFileType(int mode){
+    char fileType;
+
+    switch (mode & S_IFMT) {
+        case S_IFBLK:
+            fileType = 'b';
+            break;
+        case S_IFCHR:
+            fileType = 'c';
+            break;
+        case S_IFDIR:
+            fileType = 'd';
+            break;
+        case S_IFLNK:
+            fileType = 'l';
+            break;
+        case S_IFREG:
+            fileType = '-';
+            break;
+        case S_IFSOCK:
+            fileType = 's';
+            break;
+        case S_IFIFO:
+            fileType = 'p';
+            break;
+        default:
+            fileType = '?';
+            break;
+    }
+
+    return fileType;
+}
+
+
