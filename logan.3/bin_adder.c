bcf54636416aac94d22756432800416674380ceb
Logan Pettit <logan@hoare7.cs.umsl.edu>
2020-03-11 22:42:33 -0500

    finished n/2 summation set

---
+++ logan.3/bin_adder.c
@@ -0,0 +1,100 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+
+#define SHAREDMEMKEY 0x1596
+
+
+void detachSharedMemory(long *, int, char *);
+long *connectToSharedMemory(int *, char *, int);
+
+
+int main(int argc, char* argv[]) {
+
+    int childLogicalId = atoi(argv[1]);
+    int startingIndex = atoi(argv[2]);
+    int numbersToRead = atoi(argv[3]);
+    char* executable = strdup(argv[0]);
+    char* errorString = strcat(executable, ": Error: ");
+    long* sharedMemoryPtr;
+    int sharedMemoryId;
+    int originalIndex = startingIndex;
+    int accumulator = 0;
+
+    printf("Child logical id: %d\n\n", childLogicalId);
+    printf("Starting index: %d\n\n", startingIndex);
+
+    sharedMemoryPtr = connectToSharedMemory(&sharedMemoryId, errorString, numbersToRead);
+
+
+    int x;
+    for(x = 0; x < numbersToRead; x++){
+
+        accumulator += sharedMemoryPtr[startingIndex];
+        startingIndex += 1;
+    }
+
+
+
+
+
+    sharedMemoryPtr[originalIndex] = accumulator;
+    printf("Placing: %d into: %d\n", accumulator, originalIndex);
+
+    int t;
+    for(t = 0; t < 8; t++){
+        printf("shared memory pointer: %ld\n", sharedMemoryPtr[t]);
+    }
+
+
+    detachSharedMemory(sharedMemoryPtr, sharedMemoryId, errorString);
+
+    return 0;
+}
+
+/*Function to connect to shared memory and error check*/
+long *connectToSharedMemory(int *sharedMemoryID, char *error, int numbersToRead) {
+    char errorArr[200];
+    long *sharedMemoryPtr;
+
+    /*Get shared memory Id using shmget*/
+    *sharedMemoryID = shmget(SHAREDMEMKEY, sizeof(int) * numbersToRead, 0777 | IPC_CREAT);
+
+    /*Error check shmget*/
+    if (sharedMemoryID == (void *) -1) {
+        snprintf(errorArr, 100, "\n\n%s PID: %ld Failed to create shared memory ", error, (long) getpid());
+        perror(errorArr);
+        exit(EXIT_FAILURE);
+    }
+
+
+    /*Attach to shared memory using shmat*/
+    sharedMemoryPtr = shmat(*sharedMemoryID, NULL, 0);
+
+
+    /*Error check shmat*/
+    if (sharedMemoryPtr == (void *) -1) {
+        snprintf(errorArr, 200, "\n\n%s PID: %ld Failed to create shared memory ", error, (long) getpid());
+        perror(errorArr);
+        exit(EXIT_FAILURE);
+    }
+
+    return sharedMemoryPtr;
+}
+
+
+/*Function to detach from shared memory and error check*/
+void detachSharedMemory(long *sharedMemoryPtr, int sharedMemoryId, char *error) {
+    char errorArr[200];
+
+    if (shmdt(sharedMemoryPtr) == -1) {
+        snprintf(errorArr, 200, "\n\n%s PID: %ld Failed to create shared memory ", error, (long) getpid());
+        perror(errorArr);
+        exit(EXIT_FAILURE);
+    }
+}
--- logan.3/master.c
+++ logan.3/master.c
@@ -16,7 +16,7 @@
 
 struct Node {
     int data;
-    struct Node* next;
+    struct Node *next;
 };
 
 
@@ -24,31 +24,45 @@ jmp_buf ctrlCjmp;
 jmp_buf timerjmp;
 
 
-void detachSharedMemory(long*, int, char*);
-long*  connectToSharedMemory(int*, char*, int);
-void push(struct Node**, int );
-void deleteNode(struct Node**, int);
+void detachSharedMemory(long *, int, char *);
+
+long *connectToSharedMemory(int *, char *, int);
+
+void push(struct Node **, int);
+
+pid_t launchProcess(char *, int, int, int);
+
+void deleteNode(struct Node **, int);
+
 void ctrlCHandler();
+
 void timerHandler();
+
 unsigned int startTimer(int);
 
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
 
-    char* inputFile = "input.dat";
-    char* generatedFile = "input.dat";
-    char* output_log = "adder_log";
+    char *inputFile = "input.dat";
+    char *generatedFile = "input.dat";
+    char *output_log = "adder_log";
+    char *executable = strdup(argv[0]);
+    char *errorString = strcat(executable, ": Error: ");
     char line[51] = {0};
-    FILE* input;
-    FILE* output;
+    FILE *input;
+    FILE *output;
     int lineCounter = 0;
     int seconds = 100;
-    char* executable = strdup(argv[0]);
-    char* errorString = strcat(executable, ": Error: ");
+    int statusCode = 0;
+    int childLogicalID = 0;
+    int fileData;
+    int numbersToAdd = 2;
+    int originalFileData;
     int sharedMemoryId;
-    long* sharedMemoryPtr;
-    struct Node* childPIDs = NULL;
+    long *sharedMemoryPtr;
+    struct Node *childPIDs = NULL;
+
 
 
 
@@ -61,7 +75,7 @@ int main(int argc, char* argv[]) {
 
     /*Catch alarm generated from timer
  *      * and handle with timer Handler */
-    signal (SIGALRM, timerHandler);
+    signal(SIGALRM, timerHandler);
 
 
     /*Jump back to main enviroment where children are launched
@@ -99,15 +113,16 @@ int main(int argc, char* argv[]) {
     /*Generate input.dat*/
     output = fopen(generatedFile, "w");
 
-    if(output == NULL){
+    if (output == NULL) {
         fprintf(stderr, "output file could not be opened");
         return 1;
     }
 
     int x;
-    for(x = 0; x < 65; x++) {
+    for (x = 1; x < 9; x++) {
         fprintf(output, "%d\n", x);
     }
+
 
     fclose(output);
 
@@ -119,13 +134,17 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    while(fgets(line, sizeof(line), input) != NULL) {
+    while (fgets(line, sizeof(line), input) != NULL) {
         lineCounter += 1;
 
     }
+
+    originalFileData = lineCounter;
+    fileData = originalFileData;
+
     fclose(input);
 
-    printf("%d", lineCounter);
+    printf("line counter: %d\n", lineCounter);
 
 
     /*Start the  main timer*/
@@ -143,29 +162,132 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    char* endPointer;
+    char *endPointer;
     long fileDigit = 0;
     int i;
-    for(i = 0; i < lineCounter; i++){
-      /* different way to get digit from file
- *         fscanf(input, "%d", &d);
- *                 printf("whole line %d\n", d);*/
+    for (i = 0; i < lineCounter; i++) {
+        /* different way to get digit from file
+ *           fscanf(input, "%d", &d);
+ *                     printf("whole line %d\n", d);*/
 
         fgets(line, sizeof(line), input);
         fileDigit = strtol(line, &endPointer, 10);
-        printf("whole line: %ld\n", fileDigit);
+        /*printf("whole line: %ld\n", fileDigit);*/
 
         sharedMemoryPtr[i] = fileDigit;
     }
 
+    fclose(input);
 
-    /*test shared memory array*/
-    int t;
-    for(t = 0; t < lineCounter; t++){
-        printf("shared memory pointer: %ld\n", sharedMemoryPtr[t]);
+    /*test shared memory array
+ *     int t;
+ *         for(t = 0; t < fileData; t++){
+ *                 printf("shared memory pointer: %ld\n", sharedMemoryPtr[t]);
+ *                     }*/
+
+
+    /*n/2 summation launch*/
+    int childrenToLaunch = fileData / 2;
+    int index = 0;
+    int numberDistance = 2;
+
+
+    /* printf("children to launch before %d\n", childrenToLaunch);
+ *  
+ *       while (childrenToLaunch > 0) {
+ *        
+ *                 while (index < fileData) {
+ *                  
+ *                   
+ *                                pid = launchProcess(errorString, childLogicalID, index, numbersToAdd);
+ *                                             push(&childPIDs, pid);
+ *                                                          index += numberDistance;
+ *                                                                       childLogicalID += 1;
+ *                                                                        
+ *                                                                                     pid = wait(&statusCode);
+ *                                                                                      
+ *                                                                                                   if (pid > 0) {
+ *                                                                                                                    fprintf(stderr, "Child with PID %ld exited with status 0x%x\n",
+ *                                                                                                                                             (long) pid, statusCode);
+ *                                                                                                                                                          }
+ *                                                                                                                                                                   }*/
+
+    /*handle shifting of elements*/
+    /*sharedMemoryPtr[1] = sharedMemoryPtr[numberDistance];
+ *
+ *     int k;
+ *         for (k = 2; k < childrenToLaunch; k++) {
+ *
+ *                 sharedMemoryPtr[k] = sharedMemoryPtr[k * 2];
+ *                     }
+ *
+ *                         index = 0;
+ *                             childrenToLaunch /= 2;
+ *                                 fileData /= 2;
+ *                                     printf("LAYER FINISHED children to launch %d\n\n\n", childrenToLaunch);
+ *
+ *                                         *//*int t;
+        for (t = 0; t < 8; t++) {
+            printf("MASTER: shared memory pointer: %ld\n", sharedMemoryPtr[t]);
+        }
+    }*/
+
+    printf("\nSummed of data in file: %ld\n\n", sharedMemoryPtr[0]);
+
+
+    /*Log summation launch*/
+    printf("\n\n\nRUNNING LOG SUMMATION: \n");
+
+    double logChildrenToLaunch = 0;
+    fileData = originalFileData;
+    logChildrenToLaunch = ceil((double) fileData / log2((double) fileData));
+    index = 0;
+    double logNumberDistance;
+    logNumberDistance = logChildrenToLaunch;
+    int logNumbersToAdd = (int) logNumberDistance;
+
+
+    printf("log children to launch %f", logChildrenToLaunch);
+
+
+    while (childrenToLaunch > 0) {
+
+        while (index < fileData) {
+
+            pid = launchProcess(errorString, childLogicalID, index, logNumbersToAdd);
+            push(&childPIDs, pid);
+            index += (int) logNumberDistance;
+            childLogicalID += 1;
+
+            pid = wait(&statusCode);
+
+
+            if (pid > 0) {
+                fprintf(stderr, "Child with PID %ld exited with status 0x%x\n",
+                        (long) pid, statusCode);
+            }
+        }
+
+        /*handle shifting of elements*/
+        sharedMemoryPtr[1] = sharedMemoryPtr[numberDistance];
+
+        int k;
+        for (k = 2; k < childrenToLaunch; k++) {
+
+            sharedMemoryPtr[k] = sharedMemoryPtr[k * 2];
+        }
+
+        index = 0;
+        childrenToLaunch /= 2;
+        fileData /= 2;
+        printf("LAYER FINISHED children to launch %d\n\n\n", childrenToLaunch);
+
+        int t;
+        for (t = 0; t < 8; t++) {
+            printf("MASTER: shared memory pointer: %ld\n", sharedMemoryPtr[t]);
+        }
     }
 
-    fclose(input);
 
     detachSharedMemory(sharedMemoryPtr, sharedMemoryId, errorString);
 
@@ -173,9 +295,9 @@ int main(int argc, char* argv[]) {
 }
 
 /*Function to connect to shared memory and error check*/
-long* connectToSharedMemory( int* sharedMemoryID, char* error, int lineCount){
+long *connectToSharedMemory(int *sharedMemoryID, char *error, int lineCount) {
     char errorArr[200];
-    long* sharedMemoryPtr;
+    long *sharedMemoryPtr;
 
     /*Get shared memory Id using shmget*/
     *sharedMemoryID = shmget(SHAREDMEMKEY, sizeof(int) * lineCount, 0777 | IPC_CREAT);
@@ -193,7 +315,7 @@ long* connectToSharedMemory( int* sharedMemoryID, char* error, int lineCount){
 
 
     /*Error check shmat*/
-    if(sharedMemoryPtr == (void*) -1){
+    if (sharedMemoryPtr == (void *) -1) {
         snprintf(errorArr, 200, "\n\n%s PID: %ld Failed to create shared memory ", error, (long) getpid());
         perror(errorArr);
         exit(EXIT_FAILURE);
@@ -203,12 +325,11 @@ long* connectToSharedMemory( int* sharedMemoryID, char* error, int lineCount){
 }
 
 
-
 /*Function to detach from shared memory and error check*/
-void detachSharedMemory (long* sharedMemoryPtr, int sharedMemoryId, char* error){
+void detachSharedMemory(long *sharedMemoryPtr, int sharedMemoryId, char *error) {
     char errorArr[200];
 
-    if(shmdt(sharedMemoryPtr) == -1) {
+    if (shmdt(sharedMemoryPtr) == -1) {
         snprintf(errorArr, 200, "\n\n%s PID: %ld Failed to create shared memory ", error, (long) getpid());
         perror(errorArr);
         exit(EXIT_FAILURE);
@@ -219,30 +340,32 @@ void detachSharedMemory (long* sharedMemoryPtr, int sharedMemoryId, char* error)
 }
 
 /*Function to launch child processes and return the corresponding pid's*/
-pid_t launchProcess(char* error, int childLogicalID, int prime){
+pid_t launchProcess(char *error, int childLogicalID, int index, int numbers) {
     char errorArr[200];
     pid_t pid;
 
     char childLogicalString[20];
-    char primeString[20];
+    char indexString[20];
+    char numberString[20];
 
     snprintf(childLogicalString, 20, "%d", childLogicalID);
-    snprintf(primeString, 20, "%d", prime);
+    snprintf(indexString, 20, "%d", index);
+    snprintf(numberString, 20, "%d", numbers);
 
 
     /*Fork process and capture pid*/
     pid = fork();
 
-    if(pid == -1){
+    if (pid == -1) {
         snprintf(errorArr, 200, "\n\n%s Fork execution failure", error);
         perror(errorArr);
         exit(EXIT_FAILURE);
     }
 
-    if(pid == 0){
+    if (pid == 0) {
         /*executes files, Name of file to be execute, list of args following,
  *          * must be terminated with null*/
-        execl("./prime", "prime", childLogicalString, primeString, NULL);
+        execl("./bin_adder", "bin_adder", childLogicalString, indexString, numberString, NULL);
 
         snprintf(errorArr, 200, "\n\n%s execution of user subprogram failure ", error);
         perror(errorArr);
@@ -255,32 +378,29 @@ pid_t launchProcess(char* error, int childLogicalID, int prime){
 /**Implementation from geeksforgeeks.com**/
 /* Given a reference (pointer to pointer) to the head of a list
  *    and an int,  inserts a new node on the front of the list. */
-void push(struct Node** head_ref, int new_data)
-{
+void push(struct Node **head_ref, int new_data) {
     /* 1. allocate node */
-    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    struct Node *new_node = (struct Node *) malloc(sizeof(struct Node));
 
     /* 2. put in the data  */
-    new_node->data  = new_data;
+    new_node->data = new_data;
 
     /* 3. Make next of new node as head */
     new_node->next = (*head_ref);
 
     /* 4. move the head to point to the new node */
-    (*head_ref)    = new_node;
+    (*head_ref) = new_node;
 }
 
 /**Implementation from geeksforgeeks.com**/
 /* Given a reference (pointer to pointer) to the head of a list
  *    and a key, deletes the first occurrence of key in linked list */
-void deleteNode(struct Node **head_ref, int key)
-{
+void deleteNode(struct Node **head_ref, int key) {
     /* Store head node*/
-    struct Node* temp = *head_ref, *prev;
+    struct Node *temp = *head_ref, *prev;
 
     /* If head node itself holds the key to be deleted*/
-    if (temp != NULL && temp->data == key)
-    {
+    if (temp != NULL && temp->data == key) {
         *head_ref = temp->next;   /* Changed head */
         free(temp);               /* free old head */
         return;
@@ -288,8 +408,7 @@ void deleteNode(struct Node **head_ref, int key)
 
     /* Search for the key to be deleted, keep track of the*/
     /* previous node as we need to change 'prev->next' */
-    while (temp != NULL && temp->data != key)
-    {
+    while (temp != NULL && temp->data != key) {
         prev = temp;
         temp = temp->next;
     }
@@ -310,7 +429,7 @@ unsigned int startTimer(int seconds) {
     new.it_interval.tv_sec = 0;
     new.it_value.tv_usec = 0;
     new.it_value.tv_sec = (long int) seconds;
-    if (setitimer (ITIMER_REAL, &new, &old) < 0)
+    if (setitimer(ITIMER_REAL, &new, &old) < 0)
         return 0;
     else
         return old.it_value.tv_sec;
@@ -318,7 +437,7 @@ unsigned int startTimer(int seconds) {
 
 /*Function handles ctrl c signal and jumps to top
  *  * of program stack enviroment*/
-void ctrlCHandler(){
+void ctrlCHandler() {
     char errorArr[200];
     snprintf(errorArr, 200, "\n\nCTRL+C signal caught, killing all processes and releasing shared memory.");
     perror(errorArr);
@@ -327,7 +446,7 @@ void ctrlCHandler(){
 
 /*Function handles timer alarm signal and jumps
  *  * to top of program stack enviroment*/
-void timerHandler(){
+void timerHandler() {
     char errorArr[200];
     snprintf(errorArr, 200, "\n\ntimer interrupt triggered, killing all processes and releasing shared memory.");
     perror(errorArr);
